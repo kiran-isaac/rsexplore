81f6c4afece0357ded1446c507d5a30686cbed5e
kiran <kiranisturt@outlook.com>
2023-08-02 23:59:58 +0100

    displays number of folders and files in cwd

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{env, fs};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+async fn test() -> Result<usize, String> {
+  let current_dir = env::current_dir().map_err(|e| e.to_string())?;
+  let current_dir_str = current_dir.to_str().ok_or_else(|| "Invalid Unicode in path".to_string())?;
+
+  let mut count = 0;
+  for entry in fs::read_dir(current_dir_str).map_err(|e| e.to_string())? {
+    count += 1;
+  }
+  Ok(count)}
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![test])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
